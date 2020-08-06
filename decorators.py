f9255a561264dabbfc645841ffa4d2684bd768bf
Stan SKY <sky012877@gmail.com>
2020-08-06 14:33:12 -0700

    feat: add exception handling decorators

---
+++ decorators.py
@@ -0,0 +1,13 @@
+import sys
+from say import Say
+
+
+def try_decor(func):            # Decorator for handling exceptions
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            msg = f'ERROR!!! - Cant execute function: , {func} \n{repr(e)} {sys.exc_info()[0]}'
+            Say(msg).prn_err()
+
+    return inner
