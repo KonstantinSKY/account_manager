589807e04b441306a8f582d218a742960e54841c
Stan SKY <sky012877@gmail.com>
2020-08-10 14:22:47 -0700

    refactor: integrate logging and exception handling decorators

--- databases.py
+++ databases.py
@@ -1,19 +1,27 @@
 import sqlite3
+from decorators import try_decor
+from say import Say
 
 class ConnectDB(sqlite3.Connection):
-    """Basic SQLite connection wrapper."""
+    """Extended SQLite connection wrapper with logging and decorators."""
     def __init__(self, db_file):
         super().__init__(db_file)
         self.cur = self.cursor()
 
+    @try_decor
     def create_table(self, table, fields):
         self.cur.execute(f"CREATE TABLE IF NOT EXISTS {table} ({fields})")
+        Say(f"Created the table: {table}").prn_ok()
         return True
 
+    @try_decor
     def select_all(self, table):
         self.cur.execute(f"SELECT * FROM {table}")
-        return self.cur.fetchall()
+        records = self.cur.fetchall()
+        Say(f"Got records: {len(records)}").prn_ok()
+        return records
 
+    @try_decor
     def insert(self, table, data_obj):
         fields = ", ".join(data_obj.keys())
         binds = ", ".join(["?"] * len(data_obj))
