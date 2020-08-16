182a63422054ea913903c090e5e71906da3fa647
Stan SKY <sky012877@gmail.com>
2020-08-16 11:23:45 -0700

    test: implement core functional unit tests

--- tests/test_databases.py
+++ tests/test_databases.py
@@ -1,41 +1,12 @@
 import unittest
 from databases import ConnectDB
 
-import sqlite3
+class TestDatabases(unittest.TestCase):
+    def test_connection_init(self):
+        # We use memory for tests
+        conn = ConnectDB(":memory:")
+        self.assertIsNotNone(conn)
+        conn.close()
 
-# from init_app import *
-
-db_file = "../../DB/accounts.sqlite"
-conn = ConnectDB(db_file)
-
-
-class DatabasesTestCase(unittest.TestCase):
-
-    def test_insert(self):
-        conn.insert("services", {
-                        "name": "mail.ru",
-                        "url": "https://www.mail.ru",
-                        "description": "Email service, Russia"
-                    })
-
-
-unittest.main()
-
-conn = ConnectDB(db_file)
-curs = conn.cursor()
-print(type(conn.cursor))
-print(type(conn.cur))
-
-conn2 = sqlite3.connect(db_file)
-curs2 = conn2.cursor()
-print(type(curs))
-print(dir(curs2))
-
-print(type(curs2))
-print(dir(curs2))
-
-print(type(conn))
-print(dir(conn))
-
-print(type(conn2))
-print(dir(conn2))
+if __name__ == "__main__":
+    unittest.main()
