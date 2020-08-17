8da1a0a229e5b6b5b2bab37518539a827eac65b2
Sky <sky012877@gmail.com>
2020-08-17 09:30:15 -0700

    refactor(services): implement dependency injection and remove global state

--- services.py
+++ services.py
@@ -1,27 +1,49 @@
 from typing import Dict, Any, Optional
-from init_app import conn
+from databases import ConnectDB
 
 class Service:
-    """Business logic for web services management."""
+    """
+    Business logic for web services management.
+    Follows Active Record pattern with Dependency Injection support.
+    """
     services: Dict[int, 'Service'] = {}
     table: str = 'services'
 
-    def __init__(self, service_obj: Dict[str, Any], id_: Optional[int] = None):
+    def __init__(self, service_obj: Dict[str, Any], id_: Optional[int] = None, db: Optional[ConnectDB] = None):
+        """
+        Initialize a Service object.
+        :param service_obj: Dictionary containing service data (name, url, description).
+        :param id_: Optional ID if loading from DB.
+        :param db: Database connection instance (required for persistence).
+        """
         self.id_ = id_
-        self.name: str = service_obj["name"]
-        self.url: str = service_obj["url"]
-        self.description: str = service_obj["description"]
+        self.name: str = service_obj.get("name", "")
+        self.url: str = service_obj.get("url", "")
+        self.description: str = service_obj.get("description", "")
         
-        if not self.id_:
-            self.id_ = conn.insert(Service.table, service_obj)
+        # Persistence Logic
+        if db and not self.id_:
+            self.id_ = db.insert(Service.table, service_obj)
+            if self.id_:
+                db.commit()
         
+        # Cache Update
         if self.id_:
-            conn.commit()
-            Service.services.update({self.id_: self})
+            Service.services[self.id_] = self
+
+    def __repr__(self) -> str:
+        return f"<Service(id={self.id_}, name='{self.name}')>"
 
     @classmethod
     def show_all(cls) -> None:
         """Display all services in a clean format."""
-        for key, service in cls.services.items():
-            print("="*80)
-            print(f"[{service.id_}] {service.name} | {service.url}")
+        if not cls.services:
+            print("No services found.")
+            return
+
+        print("\n" + "="*80)
+        print(f"{'ID':<5} | {'Name':<20} | {'URL':<40}")
+        print("-" * 80)
+        for _, service in cls.services.items():
+            print(f"{service.id_:<5} | {service.name:<20} | {service.url:<40}")
+        print("="*80 + "\n")
