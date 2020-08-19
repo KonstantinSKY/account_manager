327b2b5acc8e35d2ded8cf4cad68fc1c48c86d8d
Sky <sky012877@gmail.com>
2020-08-18 17:45:10 -0700

    feat: finalize main application entry point with interactive CLI loop

--- app.py
+++ app.py
@@ -1,28 +1,65 @@
+import sys
+from init_app import conn
 from services import Service
 from accounts import Account
 from persons import Person
 from menu import Menu
-import sys
+
+def load_data() -> None:
+    """Load existing records from database into memory."""
+    # Load Services
+    for row in conn.select_all(Service.table):
+        # row: (id, name, url, description)
+        Service({"name": row[1], "url": row[2], "description": row[3]}, id_=row[0])
+    
+    # Load Persons
+    for row in conn.select_all(Person.table):
+        # row: (id, f_name, s_name, country)
+        Person({"f_name": row[1], "s_name": row[2], "country": row[3]}, id_=row[0])
+
+    # Load Accounts
+    for row in conn.select_all(Account.table):
+        # row: (id, login, password, id_service)
+        Account({"login": row[1], "password": row[2], "id_service": row[3]}, id_=row[0])
 
 def main() -> None:
-    """Main application entry point."""
-    print("Account Manager v1.0 Initializing...")
+    """Main application entry point and command loop."""
+    print("\n" + "*" * 40)
+    print("      ACCOUNT MANAGER v1.1 PRO      ")
+    print("*" * 40)
     
-    # Setup Main Menu
+    # Initialization
+    load_data()
+    
+    # Menu Configuration
     main_menu = Menu("main", {
-        "1": "Show Services",
-        "2": "Show Persons",
-        "3": "Show Accounts",
-        "q": "Exit"
+        "1": "List All Services",
+        "2": "List All Persons",
+        "3": "List All Accounts",
+        "q": "Exit Application"
     })
     
-    # Map Actions
-    main_menu.update_action("1", Service.show_all)
-    main_menu.update_action("2", Person.show_all)
-    main_menu.update_action("3", Account.show_all)
+    # Action Mapping
+    actions = {
+        "1": Service.show_all,
+        "2": Person.show_all,
+        "3": Account.show_all,
+        "q": lambda: sys.exit("Exiting gracefully...")
+    }
     
-    print("Application Ready.")
-    # In a real app, we would run a loop here.
+    # Application Loop
+    while True:
+        main_menu.display()
+        choice = input("\nSelect an option: ").strip().lower()
+        
+        if choice in actions:
+            actions[choice]()
+        else:
+            print(f"Invalid option: '{choice}'. Please try again.")
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nInterrupted by user. Goodbye.")
+        sys.exit(0)
